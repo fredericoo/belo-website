51b728750353e2b82a00683615ed4ad64d30d6a6
Frederico Batista <frederico@Fredericos-MBP.station>
2021-01-22 12:42:31 +0100

    implementing api fetching for market

--- components/Ticker/Ticker.js
+++ components/Ticker/Ticker.js
@@ -1,80 +1,45 @@
 import styles from "./Ticker.module.scss";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 const TickerEntry = ({ entry }) => {
-	const positive = entry.value > 0;
+	const positive = entry.change > 0;
 	return (
 		<li className={`s-xs ${styles.entry} ${positive && styles.positive}`}>
-			<div className={`${styles.label}`}>{entry.label}</div>
-			<div className={`${styles.marketPrice}`}>{entry.marketPrice}</div>
+			<div className={`${styles.label}`}>{entry.symbol}</div>
+			<div className={`${styles.marketPrice}`}>{entry.price}</div>
 			<div className={styles.arrow}></div>
 			<div className={`${styles.value}`}>
-				{entry.value}&emsp;
-				{(
-					100 -
-					((entry.marketPrice + entry.value) * 100) / entry.marketPrice
-				).toFixed(2)}
-				%
+				{entry.change}&emsp;{entry.changePercent}
 			</div>
 		</li>
 	);
 };
 
 const Ticker = ({}) => {
-	const data = [
-		{
-			label: "Euro Stoxx 50 Pr",
-			marketPrice: 3602.67,
-			value: 3.12,
-		},
-		{
-			label: "Euro Stoxx 50 Pr",
-			marketPrice: 3602.67,
-			value: -3.12,
-		},
-		{
-			label: "Euro Stoxx 50 Pr",
-			marketPrice: 3602.67,
-			value: 3.12,
-		},
-		{
-			label: "Euro Stoxx 50 Pr",
-			marketPrice: 3602.67,
-			value: -3.12,
-		},
-		{
-			label: "Euro Stoxx 50 Pr",
-			marketPrice: 3602.67,
-			value: 3.12,
-		},
-		{
-			label: "Euro Stoxx 50 Pr",
-			marketPrice: 3602.67,
-			value: -3.12,
-		},
-		{
-			label: "Euro Stoxx 50 Pr",
-			marketPrice: 3602.67,
-			value: 3.12,
-		},
-		{
-			label: "Euro Stoxx 50 Pr",
-			marketPrice: 3602.67,
-			value: -3.12,
-		},
-	];
+	const [data, setData] = useState([]);
+	useEffect(() => {
+		fetch("/api/market")
+			.then((res) => res.json())
+			.then((data) => {
+				if (data.stocks) setData(data.stocks);
+			});
+	}, []);
 
 	return (
 		<aside className={styles.ticker}>
 			<div className={styles.viewport} style={{ "--entries": data.length }}>
 				<ul className={styles.first}>
-					{data.map((entry, index) => (
-						<TickerEntry key={`entry-${index}`} entry={entry} />
-					))}
+					{data.length &&
+						data.map((entry, index) => (
+							<TickerEntry key={`entry-${index}`} entry={entry} />
+						))}
 				</ul>
 				<ul className={styles.second}>
-					{data.map((entry, index) => (
-						<TickerEntry key={`entry-${index}`} entry={entry} />
-					))}
+					{data.length &&
+						data.map((entry, index) => (
+							<TickerEntry key={`entry-${index}`} entry={entry} />
+						))}
 				</ul>
 			</div>
 		</aside>
---
+++ pages/api/market.js
@@ -0,0 +1,42 @@
+async function market(req, res) {
+	const apiKey = process.env.ALPHAVANTAGE_KEY;
+
+	const symbols = ["^GSPC", "AAPL", "FB"];
+
+	await Promise.all(
+		symbols.map((symbol) =>
+			fetch(
+				`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&apikey=${apiKey}&symbol=${symbol}`
+			)
+		)
+	)
+		.then(function (responses) {
+			return Promise.all(
+				responses.map(function (response) {
+					return response.json();
+				})
+			);
+		})
+		.then(function (data) {
+			const apiRes = { stocks: [] };
+			data.forEach((stock) => {
+				if (stock["Global Quote"]) {
+					apiRes.stocks.push({
+						symbol: stock["Global Quote"]["01. symbol"],
+						price: stock["Global Quote"]["05. price"],
+						change: stock["Global Quote"]["09. change"],
+						changePercent: stock["Global Quote"]["10. change percent"],
+					});
+				}
+			});
+			res.setHeader("Cache-Control", "s-maxage=180, stale-while-revalidate");
+			res.json(apiRes);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.setHeader("Cache-Control", "s-maxage=60, stale-while-revalidate");
+			res.json({ error: "Error fetching data from the API." });
+		});
+}
+
+export default market;
