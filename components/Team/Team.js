f0583024e702104196fdfa6ffe962783aee61fcb
Frederico Batista <frederico@Fredericos-MBP.station>
2021-01-24 15:52:02 +0100

    moving from SWR to getStaticProps for SEO

--- components/Team/Team.js
+++ components/Team/Team.js
@@ -2,26 +2,14 @@ import styles from "./Team.module.scss";
 import { useRouter } from "next/router";
 import TeamMember from "components/TeamMember/TeamMember";
 
-import useSWR from "swr";
-import { fetcher } from "utils/fetcher";
-
-const Team = () => {
+const Team = ({ members }) => {
 	const { locale } = useRouter();
-	const { data } = useSWR(
-		JSON.stringify({ docType: "member", locale }),
-		fetcher,
-		{
-			revalidateOnFocus: false,
-			revalidateOnMount: true,
-			revalidateOnReconnect: false,
-		}
-	);
 
 	return (
 		<section className={`${styles.section} container`}>
 			<div className={`${styles.members} loop loop--sm`}>
-				{data &&
-					data.map((member) => (
+				{members &&
+					members.map((member) => (
 						<TeamMember
 							key={member.uid}
 							name={member.name}
--- pages/team.js
+++ pages/team.js
@@ -2,13 +2,40 @@ import Layout from "components/Layout/Layout";
 import Team from "components/Team/Team";
 import Meta from "components/Meta/Meta";
 import useTranslation from "next-translate/useTranslation";
+import { queryRepeatableDocuments } from "utils/queries";
 
-export default function Page() {
+export default function Page({ docs }) {
 	const { t } = useTranslation();
 	return (
 		<Layout>
 			<Meta pageTitle={t("common:menu.team")} />
-			<Team perPage={999} />
+			<Team
+				members={docs.map((doc) => {
+					return {
+						uid: doc.slugs[0],
+						name: doc.data.name,
+						position: doc.data.position,
+						photo: doc.data.photo,
+						bio: doc.data.bio,
+					};
+				})}
+			/>
 		</Layout>
 	);
+}
+
+export async function getStaticProps({ locale }) {
+	const documents = await queryRepeatableDocuments(
+		(doc) => doc.type === "member" && doc.lang === locale
+	);
+
+	if (documents) {
+		return {
+			revalidate: 60,
+			props: {
+				docs: documents || {},
+			},
+		};
+	}
+	return { revalidate: 600, props: { docs: {} } };
 }
