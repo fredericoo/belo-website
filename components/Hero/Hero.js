1374f0dc4dc7413cf9f5b7559b9474a72bce2478
Frederico Batista <frederico@Fredericos-MBP.station>
2021-01-24 20:20:26 +0100

    pulling data from prismic

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,8 +1,9 @@
 import styles from "./Hero.module.scss";
 import { useState, useEffect } from "react";
 import Button from "components/Button/Button";
+import { RichText } from "prismic-reactjs";
 
-const Hero = ({ heading }) => {
+const Hero = ({ heading, about, cta }) => {
 	const [isLoaded, setIsLoaded] = useState(false);
 	useEffect(() => {
 		setIsLoaded(true);
@@ -14,16 +15,11 @@ const Hero = ({ heading }) => {
 				className={`${styles.canvas} ${isLoaded ? styles.loaded : ""}`}
 			></div>
 			<div className={styles.overlay}></div>
-			<h1 className={`h-1 ${styles.heading}`}>{heading}</h1>
+			<h1 className={`h-1 ${styles.heading}`}>{RichText.asText(heading)}</h1>
 			<div className={`body body--sans ${styles.box}`}>
-				<p>
-					A Belo Investment Research une expertise acadêmica e tecnologia de
-					ponta na investigação de fontes, coleta, seleção, cruzamento e análise
-					de dados alternativos e tradicionais, para embasar as decisões de
-					investidores institucionais.
-				</p>
+				<RichText render={about} />
 				<Button href="/about" type="arrow">
-					Saiba mais
+					{cta}
 				</Button>
 			</div>
 		</section>
