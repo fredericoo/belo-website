0bffe771d14114a5a404a2c5b5a9a4aba2cbfc01
Frederico Batista <frederico@Fredericos-MBP.station>
2021-01-24 20:20:52 +0100

    changed hero to pull data from prismic

--- pages/index.js
+++ pages/index.js
@@ -6,19 +6,39 @@ import News from "components/News/News";
 import WorldNews from "components/WorldNews/WorldNews";
 import AltData from "components/AltData/AltData";
 import Meta from "components/Meta/Meta";
-import useTranslation from "next-translate/useTranslation";
+import { Client } from "utils/prismicHelpers";
 
-export default function Home() {
-	const { t } = useTranslation();
+export default function Home({ doc }) {
 	return (
 		<Layout>
 			<Meta />
 			<Ticker />
-			<Hero heading={t("common:desc")} />
+			{doc && doc.data && (
+				<Hero
+					heading={doc.data.heading}
+					about={doc.data.about}
+					cta={doc.data.cta}
+				/>
+			)}
 			<News showDivider />
 			<WorldNews showDivider perPage={8} display={5} />
 			<AltData showDivider />
 			<Articles showDivider />
 		</Layout>
 	);
+}
+
+export async function getStaticProps({ locale }) {
+	const client = Client();
+	const doc = await client.getSingle("home", { lang: locale });
+
+	if (doc) {
+		return {
+			revalidate: 600,
+			props: {
+				doc: doc || {},
+			},
+		};
+	}
+	return { revalidate: 60, props: { doc: {} } };
 }
