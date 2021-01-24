bffd4df795c47349c16fad30e92eea3cfe17a212
Frederico Batista <frederico@Fredericos-MBP.station>
2021-01-24 18:22:17 +0100

    sorting team alphabetically

--- components/Team/Team.js
+++ components/Team/Team.js
@@ -1,23 +1,25 @@
 import styles from "./Team.module.scss";
-import { useRouter } from "next/router";
 import TeamMember from "components/TeamMember/TeamMember";
+import useTranslation from "next-translate/useTranslation";
 
 const Team = ({ members }) => {
-	const { locale } = useRouter();
-
+	const { t } = useTranslation();
 	return (
 		<section className={`${styles.section} container`}>
+			<h2 className={`${styles.heading} h-1`}>{t("common:menu.team")}</h2>
 			<div className={`${styles.members} loop loop--sm`}>
 				{members &&
-					members.map((member) => (
-						<TeamMember
-							key={member.uid}
-							name={member.name}
-							position={member.position}
-							photo={member.photo}
-							bio={member.bio}
-						/>
-					))}
+					members
+						.sort((a, b) => (a.uid > b.uid ? 1 : -1))
+						.map((member) => (
+							<TeamMember
+								key={member.uid}
+								name={member.name}
+								position={member.position}
+								photo={member.photo}
+								bio={member.bio}
+							/>
+						))}
 			</div>
 		</section>
 	);
