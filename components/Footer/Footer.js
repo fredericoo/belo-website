6b7c633270563cf5e2f15602bd6a2ed8e4f9f663
Frederico Batista <frederico@Fredericos-MBP.station>
2021-01-25 15:27:15 +0100

    adding translatable terms

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,28 +1,24 @@
 import styles from "./Footer.module.scss";
-const Footer = () => (
-	<footer className={`${styles.section} container`}>
-		<h3 className="visually-hidden">Footer</h3>
-		<ul className={`loop s-sm loop--sm ${styles.columns}`}>
-			<li>
-				<h3>Belo Investment Research</h3>
-				R. Ouro Fino 395/701. <br />
-				Belo Horizonte MG, Brazil
-			</li>
-			<li>
-				<h3>Contato</h3>
-				<p>belo@belo.re</p>
-				<p>+1 646 662 9 863</p>
-			</li>
-			<li>
-				<h3>Links</h3>
-				<p>
-					<a href="#">Cookies</a>
-				</p>
-				<p>
-					<a href="#">Privacy</a>
-				</p>
-			</li>
-		</ul>
-	</footer>
-);
+import useTranslation from "next-translate/useTranslation";
+const Footer = () => {
+	const { t } = useTranslation();
+
+	return (
+		<footer className={`${styles.section} container`}>
+			<h3 className="visually-hidden">{t("common:footer")}</h3>
+			<ul className={`loop s-sm loop--sm ${styles.columns}`}>
+				<li>
+					<h3>Belo Investment Research</h3>
+					R. Ouro Fino 395/701. <br />
+					Belo Horizonte MG, Brazil
+				</li>
+				<li>
+					<h3>{t("common:menu.contact")}</h3>
+					<p>belo@belo.re</p>
+					<p>+1 646 662 9 863</p>
+				</li>
+			</ul>
+		</footer>
+	);
+};
 export default Footer;
