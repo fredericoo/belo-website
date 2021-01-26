2a5fceb818231b0da7ed08686d7994f6fee62029
Frederico Batista <frederico@Fredericos-MBP.station>
2021-01-26 15:42:47 +0100

    added page header

--- pages/alt-data.js
+++ pages/alt-data.js
@@ -2,12 +2,20 @@ import Layout from "components/Layout/Layout";
 import AltData from "components/AltData/AltData";
 import Meta from "components/Meta/Meta";
 import useTranslation from "next-translate/useTranslation";
+import styles from "./alt-data.module.scss";
 
 export default function Page() {
 	const { t } = useTranslation();
 	return (
 		<Layout>
 			<Meta pageTitle={t("common:menu.alternativeData")} />
+			<header className={`${styles.section} grid grid--inner`}>
+				<h2 className={`${styles.heading} h-1`}>
+					{t("common:menu.alternativeData")}
+				</h2>
+
+				<div className={styles.textBlock}>Markup text</div>
+			</header>
 			<AltData perPage={9} />
 		</Layout>
 	);
