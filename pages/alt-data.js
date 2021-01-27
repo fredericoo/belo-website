c8c0efa5fd8805a1f90481cbd93ca95193310a0b
Frederico Batista <frederico@Fredericos-MBP.station>
2021-01-27 17:12:08 +0100

    added text to alt-data

--- pages/alt-data.js
+++ pages/alt-data.js
@@ -3,20 +3,48 @@ import AltData from "components/AltData/AltData";
 import Meta from "components/Meta/Meta";
 import useTranslation from "next-translate/useTranslation";
 import styles from "./alt-data.module.scss";
+import { RichText } from "prismic-reactjs";
+import { Client } from "utils/prismicHelpers";
 
-export default function Page() {
+export default function Page({ doc }) {
 	const { t } = useTranslation();
 	return (
 		<Layout>
 			<Meta pageTitle={t("common:menu.alternativeData")} />
 			<header className={`${styles.section} grid grid--inner`}>
-				<h2 className={`${styles.heading} h-1`}>
-					{t("common:menu.alternativeData")}
-				</h2>
+				<div className={`${styles.heading}`}>
+					<h2 className={`${styles.title} h-1`}>
+						{t("common:menu.alternativeData")}
+					</h2>
+					{doc && doc.data.lead && (
+						<div className={`h-3`}>
+							<RichText render={doc.data.lead} />
+						</div>
+					)}
+				</div>
 
-				<div className={styles.textBlock}>Markup text</div>
+				{doc && (
+					<div className={`${styles.textBlock} body body--sans s-sm`}>
+						<RichText render={doc.data.about} />
+					</div>
+				)}
 			</header>
 			<AltData perPage={9} />
 		</Layout>
 	);
+}
+
+export async function getStaticProps({ locale }) {
+	const client = Client();
+	const document = await client.getSingle("altdata", { lang: locale });
+
+	if (document) {
+		return {
+			revalidate: 600,
+			props: {
+				doc: document || {},
+			},
+		};
+	}
+	return { revalidate: 60, props: { doc: {} } };
 }
