24fcb48e0472d7fef39ee0bf5168d30dff2ffee3
Frederico Batista <frederico@Fredericos-MBP.station>
2021-01-29 00:05:17 +0100

    decimal and thousands separator

--- components/Ticker/Ticker.js
+++ components/Ticker/Ticker.js
@@ -1,18 +1,37 @@
 import styles from "./Ticker.module.scss";
 import { useState, useEffect } from "react";
-import { useRouter } from "next/router";
+import { withSeparator } from "./utils/numbers";
+import useTranslation from "next-translate/useTranslation";
 
 const TickerEntry = ({ entry }) => {
 	if (entry.symbol) {
+		const { t } = useTranslation();
 		const direction =
 			entry.change > 0 ? "positive" : entry.change < 0 ? "negative" : "neutral";
 		return (
 			<li className={`s-xs ${styles.entry} ${styles[direction]}`}>
 				<div className={`${styles.label}`}>{entry.name}</div>
-				<div className={`${styles.price}`}>{entry.price}</div>
+				<div className={`${styles.price}`}>
+					{withSeparator(
+						entry.price,
+						t("common:separator.decimal"),
+						t("common:separator.thousands")
+					)}
+				</div>
 				<div className={styles.arrow}></div>
 				<div className={`${styles.value}`}>
-					{entry.change}&emsp;{entry.changesPercentage}%
+					{withSeparator(
+						entry.change,
+						t("common:separator.decimal"),
+						t("common:separator.thousands")
+					)}
+					&ensp;(
+					{withSeparator(
+						entry.changesPercentage,
+						t("common:separator.decimal"),
+						t("common:separator.thousands")
+					)}
+					%)
 				</div>
 			</li>
 		);
