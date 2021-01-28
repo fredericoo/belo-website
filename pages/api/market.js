f4c879e8ec1c25921796e68e79f6cd2a948c362a
Frederico Batista <frederico@Fredericos-MBP.station>
2021-01-28 14:34:02 +0100

    s-maxage to 1 minute

--- pages/api/market.js
+++ pages/api/market.js
@@ -15,48 +15,13 @@ async function market(req, res) {
 	)
 		.then((res) => res.json())
 		.then((data) => {
-			res.setHeader("Cache-Control", "s-maxage=600, stale-while-revalidate");
+			res.setHeader("Cache-Control", "s-maxage=60, stale-while-revalidate");
 			res.json({ stocks: data });
 		})
 		.catch((err) => {
-			res.setHeader("Cache-Control", "s-maxage=600, stale-while-revalidate");
+			res.setHeader("Cache-Control", "s-maxage=60, stale-while-revalidate");
 			res.json({ err });
 		});
-
-	// 	await Promise.all(
-	// 		symbols.map((symbol) =>
-	// 			fetch(
-	// 				`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&apikey=${apiKey}&symbol=${symbol}`
-	// 			)
-	// 		)
-	// 	)
-	// 		.then(function (responses) {
-	// 			return Promise.all(
-	// 				responses.map(function (response) {
-	// 					return response.json();
-	// 				})
-	// 			);
-	// 		})
-	// 		.then(function (data) {
-	// 			const apiRes = { stocks: [] };
-	// 			data.forEach((stock) => {
-	// 				if (stock["Global Quote"]) {
-	// 					apiRes.stocks.push({
-	// 						symbol: stock["Global Quote"]["01. symbol"],
-	// 						price: stock["Global Quote"]["05. price"],
-	// 						change: stock["Global Quote"]["09. change"],
-	// 						changePercent: stock["Global Quote"]["10. change percent"],
-	// 					});
-	// 				}
-	// 			});
-	// 			res.setHeader("Cache-Control", "s-maxage=600, stale-while-revalidate");
-	// 			res.json(apiRes);
-	// 		})
-	// 		.catch((err) => {
-	// 			console.log(err);
-	// 			res.setHeader("Cache-Control", "s-maxage=600, stale-while-revalidate");
-	// 			res.json({ error: "Error fetching data from the API." });
-	// 		});
 }
 
 export default market;
