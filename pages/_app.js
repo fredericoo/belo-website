3d208ed9a52e378a83d21ccb83c8bf7589c5be44
Frederico Batista <frederico@DESKTOP-PSTEPDA.station>
2021-02-01 19:13:16 +0100

    localisation on moment.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,19 @@
 import "styles/globals.scss";
+
 import Navbar from "components/Navbar/Navbar";
 import Footer from "components/Footer/Footer";
-
 import CookieConsent from "components/CookieConsent/CookieConsent";
+
+import moment from "moment";
+
 import { AvailableLocalesContext } from "utils/context";
 import useAvailableLocales from "utils/hooks/useAvailableLocales";
+import { useRouter } from "next/router";
 
 const App = ({ Component, pageProps }) => {
+	const { locale } = useRouter();
 	const availableLocales = useAvailableLocales();
+	moment.locale(locale);
 	const NavbarComponent = Component.Navbar || Navbar;
 	const FooterComponent = Component.Footer || Footer;
 	return (
