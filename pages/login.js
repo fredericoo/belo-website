49b6a0c5824500cbac7d6f4436d04d5de74e220b
Frederico Batista <frederico@Fredericos-MBP.station>
2021-02-15 15:59:12 +0100

    added `forgot your password?`

--- pages/login.js
+++ pages/login.js
@@ -9,6 +9,19 @@ import Button from "components/Button/Button";
 const LoginPage = () => {
 	const { t } = useTranslation();
 	const [auth, setAuth] = useState();
+	const [loading, setLoading] = useState(false);
+
+	const handleAuth = (e) => {
+		e.preventDefault();
+		if (window) {
+			setAuth(null);
+			setLoading(true);
+			window.setTimeout(() => {
+				setLoading(false);
+				setAuth(false);
+			}, 300 + Math.random() * 600);
+		}
+	};
 
 	return (
 		<Layout>
@@ -17,18 +30,21 @@ const LoginPage = () => {
 				<header className={styles.header}>
 					<h1 className="h-3">{t("common:login")}</h1>
 				</header>
-				<form className={styles.form}>
+				<form
+					onSubmit={handleAuth}
+					className={`${styles.form} ${loading && styles.loading}`}
+				>
 					<label className={`smcp`} htmlFor="username">
 						Email or Client ID
 					</label>
-					<input type="text" name="username" id="username" />
+					<input type="text" name="username" id="username" required />
 					<label className={`smcp`} htmlFor="password">
 						Password
 					</label>
-					<input type="password" name="password" id="password" />
-					<Button type="primary" onClick={() => setAuth(false)}>
-						Proceed
-					</Button>
+					<input type="password" name="password" id="password" required />
+					<button className={styles.login} type="submit">
+						{loading ? "Loading" : "Log in"}
+					</button>
 					{auth === false && (
 						<div className={`${styles.error} s-xs`}>
 							Username or password do not check out. Check your credentials and
@@ -39,6 +55,9 @@ const LoginPage = () => {
 				<div className={`${styles.footer} s-sm`}>
 					<Button type="arrowBack" href="/">
 						Back to Belo.re
+					</Button>
+					<Button type="link" href="/">
+						Forgot your password?
 					</Button>
 				</div>
 			</section>
